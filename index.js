eccc563d1166bca328797c704fa05d86a7a4dff2
briancherus <brian.cherus@student.moringaschool.comssh-keygen -t rsa -b 4096 -C brian.cherus@student.moringaschool.com>
2023-09-25 11:54:10 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+// writeCards function
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (const name of names) {
+      thankYouMessages.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  // countDown function
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  
